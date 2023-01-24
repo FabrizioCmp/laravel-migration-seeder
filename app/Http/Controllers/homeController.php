464ee00f97b19716640a49debc66224e00f31f46
FabrizioCmp <fabriziocampari01@gmail.com>
2023-01-24 14:18:46 +0100

    adds migration create_trains_table

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class homeController extends Controller
 {
-    //
+    function index(){
+        
+        
+        return view('homepage');
+    }
 }
